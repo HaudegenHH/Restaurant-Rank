fc08d8850e7093613eb58755661ff33098fdd328
HaudegenHH <einsundeins79@gmx.de>
2022-09-14 20:28:17 +0200

    final commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="container">    
+      <h1>Welcome to the restaurant application!</h1>
+      <NuxtLink to="/restaurants">Go to restaurants</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .container {
+    text-align: center;
+    margin-top: 5rem;
+  }
+</style>
---
+++ pages/restaurants/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>    
+    <NuxtLayout name="custom">
+      <div class="container">      
+        <RestaurantTable />      
+      </div>
+    </NuxtLayout>
+  </div>
+</template>
+
+<style scoped>
+  .container {
+    text-align: center;
+    margin-top: 5rem;
+  }
+</style>
